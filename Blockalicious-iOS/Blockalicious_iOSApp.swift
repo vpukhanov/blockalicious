4ab5ac8f6d8710478fa6bce674c034011b977e57
Vyacheslav Pukhanov <vyacheslav@hey.com>
2022-06-10 16:12:55 +0300

    generate iOS app boilerplate

---
+++ Blockalicious-iOS/Blockalicious_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blockalicious_iOSApp.swift
+//  Blockalicious-iOS
+//
+//  Created by Vyacheslav Pukhanov on 10.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Blockalicious_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
