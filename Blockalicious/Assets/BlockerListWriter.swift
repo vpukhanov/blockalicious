2ee8572a9ea874af3f7a3247358c8a1c073697e4
Vyacheslav Pukhanov <vyacheslav@hey.com>
2022-06-10 18:44:31 +0300

    fix iOS version bundle identifier to match macOS one

--- Blockalicious/Assets/BlockerListWriter.swift
+++ Blockalicious/Assets/BlockerListWriter.swift
@@ -3,7 +3,7 @@ import SafariServices
 
 class BlockerListWriter {
     #if os(iOS)
-    static let contentBlockerBundleId = "ru.pukhanov.Blockalicious-iOS.Content-Blocker-iOS"
+    static let contentBlockerBundleId = "ru.pukhanov.Blockalicious.Content-Blocker-iOS"
     #endif
     #if os(macOS)
     static let contentBlockerBundleId = "ru.pukhanov.Blockalicious.Content-Blocker"
