ad4e507a4a1490e9e8a3d0bd62a44aa2140fa791
Vyacheslav Pukhanov <vyacheslav@hey.com>
2022-06-12 22:50:44 +0300

    add website favicons to the macOS version

--- Blockalicious/Views/ContentView.swift
+++ Blockalicious/Views/ContentView.swift
@@ -9,6 +9,15 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Table($blockedDomainsVim.domains, selection: $selectedDomain) {
+                TableColumn("🏷️") { $item in
+                    AsyncImage(url: URL(string: item.favicon)) { image in
+                        image.resizable()
+                            .frame(width: 16, height: 16)
+                    } placeholder: {
+                        Text("🧭")
+                    }
+                }
+                .width(16)
                 TableColumn("Domain") { $item in
                     TextField("Domain Name", text: $item.name)
                         .textCase(.lowercase)
